e1bd2e9b53cf49cb5c2a9f5951c0fb6e0b8793f5
ȘERBAN PASCU <serban.pascu@stud.ubbcluj.ro>
2020-12-16 17:34:36 +0200

    Laborator 11

--- Pascu_Serban_Lab10/ListPage.xaml.cs
+++ Pascu_Serban_Lab10/ListPage.xaml.cs
@@ -32,5 +32,21 @@ namespace Pascu_Serban_Lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
